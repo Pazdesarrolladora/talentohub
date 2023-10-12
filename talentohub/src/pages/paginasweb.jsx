70d695ddf4a3ef7983c9b11849ff9d04c41f1101
Paz Valenzuela <pazvalenzu@gmail.com>
2023-10-12 00:10:54 -0300

    cambiosinconflictosidiosquiere

--- talentohub/src/pages/paginasweb.jsx
+++ talentohub/src/pages/paginasweb.jsx
@@ -10,42 +10,51 @@ const Paginasweb = () => {
       <Navbar />
       <div className="main-content">
         <Promociones />
-        <h1 className="tituloweb">Planes de E-commerce y Desarrollo Web </h1>
-        <div className="container">
-          <div className="row">
-            <div className="col-4">
-              <Cardconbotones
-                imagen={
-                  "https://i.pinimg.com/564x/f2/4c/53/f24c530147b53131e9eecbb23340de09.jpg"
-                }
-                titulo={"Presencia Online Básica"}
-                informacion={"Creamos una página web atractiva y funcional que incluye un menú adaptable, galería de imágenes, información detallada sobre la empresa o proyecto, y un formulario de contacto para facilitar la interacción. Además, integramos tu presencia en redes sociales para una mayor conectividad. Garantizamos la seguridad con un certificado SSL incluido y ofrecemos el dominio gratis por el primer año. Para que puedas administrar tu página web de manera efectiva, también proporcionamos un tutorial online gratuito."}
-                botoninfo={"¡Voy!"}
-                referencia={"referencia"}
-              />
-            </div>
-            <div className="col-4">
-              <Cardconbotones
-                imagen={
-                  "https://i.pinimg.com/564x/75/8c/47/758c47ea6cb9e77217b875c810272d25.jpg"
-                }
-                titulo={"Vende con Estilo"}
-                informacion={"Creamos una tienda online atractiva y profesional que ofrece funcionalidades avanzadas, como un carrito de compras, un catálogo de productos con descripciones detalladas, un proceso de pago seguro y una eficiente gestión de inventario. Además, garantizamos una integración completa con pasarelas de pago y redes sociales para una experiencia de compra completa. Aseguramos la seguridad de tu tienda con un certificado SSL incluido y proporcionamos el dominio gratis por el primer año. Para facilitar la administración, ofrecemos un tutorial online gratuito. ¡Haz despegar tu negocio online con nosotros! ."}
-                botoninfo={"¡Voy!"}
-                referencia={"referencia"}
-              />
-            </div>
-            <div className="col-4">
-              <Cardconbotones
-                imagen={
-                  "https://i.pinimg.com/564x/cd/a2/bd/cda2bd498a15bc4f043aea5ed40806e2.jpg"
-                }
-                titulo={"Potencia tu Negocio"}
-                informacion={"Ofrecemos un servicio de diseño completo que abarca la creación de una página web completa con tienda online, proporcionando funcionalidades premium que incluyen todas las características de nuestros servicios de página web y e-commerce. Garantizamos un proceso de pago seguro con múltiples opciones, una integración completa con pasarelas de pago y redes sociales, y extras como estrategias de marketing especializadas para potenciar tu tienda online. También nos ocupamos de la seguridad y el mantenimiento, con un certificado SSL incluido y un dominio gratuito por el primer año. Además, para facilitar la administración, ofrecemos un tutorial online gratuito. ¡Haz despegar tu presencia online con nuestra solución integral"}
-                botoninfo={"!Voy!"}
-                referencia={"referencia"}
-              />
-            </div>
+        <h1 className="tituloweb">Planes de E-commerce y Desarrollo Web</h1>
+      </div>
+      <div className="container">
+        <div className="row">
+          <div className="col-12 col-lg-4 ">
+            <Cardconbotones
+              imagen={"imagen_url"}
+              titulo={"Título del Card 1"}
+              informacion={[
+                "Información 1",
+                "Información 2",
+                "Información 3",
+                // Agrega más información si es necesario
+              ]}
+              botoninfo={"¡Voy!"}
+              referencia={"http://api.whatsapp.com/send?phone=56945111875"}
+            />
+          </div>
+          <div className="col-12 col-lg-4 ">
+            <Cardconbotones
+              imagen={"imagen_url_2"}
+              titulo={"Título del Card 2"}
+              informacion={[
+                "Otra información 1",
+                "Otra información 2",
+                "Otra información 3",
+                // Agrega más información si es necesario
+              ]}
+              botoninfo={"¡Voy!"}
+              referencia={"http://api.whatsapp.com/send?phone=56945111875"}
+            />
+          </div>
+          <div className="col-12 col-lg-4">
+            <Cardconbotones
+              imagen={"imagen_url_3"}
+              titulo={"Título del Card 3"}
+              informacion={[
+                "Más información 1",
+                "Más información 2",
+                "Más información 3",
+                // Agrega más información si es necesario
+              ]}
+              botoninfo={"¡Voy!"}
+              referencia={"http://api.whatsapp.com/send?phone=56945111875"}
+            />
           </div>
         </div>
         <div className="container">
