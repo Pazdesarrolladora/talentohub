c8c302904280f1f113a3dab7923c1a1554fd1689
Paz Valenzuela <pazvalenzu@gmail.com>
2023-10-17 23:20:04 -0300

    contacto

--- talentohub/src/pages/contacto.jsx
+++ talentohub/src/pages/contacto.jsx
@@ -5,10 +5,18 @@ const Contacto = () => {
   return (
     <>
       <Navbar />
-      <br />
-      <br />
-      <br />
-      <h1>esta es la pagina del contacto</h1>
+      <div className="container">
+          <div className="row">
+      <div className="mb-3">
+  <label for="exampleFormControlInput1" className="form-label">Email address</label>
+  <input type="email" className="form-control" id="exampleFormControlInput1" placeholder="name@example.com"/>
+</div>
+<div class="mb-3">
+  <label for="exampleFormControlTextarea1" className="form-label">Example textarea</label>
+  <textarea className="form-control" id="exampleFormControlTextarea1" rows="3"></textarea>
+</div>
+</div>
+</div>
       <Footer />
     </>
   );
